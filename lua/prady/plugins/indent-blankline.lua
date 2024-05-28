d14316961a3ad8c4e3c7b8abd0f1a857dc46dd5b
pradyumna <pradyumna.209301309@muj.manipal.edu>
2024-05-28 19:01:04 +0530

    make indent line lighter

--- lua/prady/plugins/indent-blankline.lua
+++ lua/prady/plugins/indent-blankline.lua
@@ -3,6 +3,9 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   main = "ibl",
   opts = {
-    indent = { char = "┊" },
+    indent = {
+      highlight = "IndentBlanklineChar",
+      char = "┊",
+    },
   },
 }
